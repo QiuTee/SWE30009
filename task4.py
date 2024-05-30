5a37cff795ac92357b582d9a90a3d89a050a364e
QiuTee <nqt8977@gmail.com>
2024-05-30 20:20:50 +0700

    update task 4

--- task4.py
+++ task4.py
@@ -1,58 +1,72 @@
-class Calculation:
+class Solution(object):
     def __init__(self):
-        self.symbols = ['+', '-', '*', "/"]
+        self.signs = ['+', '-', '*', "/"]
 
+    # Run the original program (correct case) - returns the correct result  
     def original_program(self, A, B):
         A = A - B
         return A * 2
 
-    def operator1(self, symbol, a, b):
-        if symbol == '+':
+    # Possible incorrect operator cases with "A = A - B ". 
+    # Returns the value of the first operation between a and b with any incorrect operators occurring      
+    def incorrect_case1(self, sign, a, b):
+        if sign == '+':
             return a + b
-        elif symbol == '-':
+        elif sign == '-':
             return a - b
-        elif symbol == '*':
+        elif sign == '*':
             return a * b
         else:
             return a / b
-
-    def operator2(self, symbol, c):
-        if symbol == '+':
+        
+    # Possible incorrect operator cases with " C = A * 2 ". 
+    # Returns the value of the second operation (with c) with any incorrect operators occurring
+    def incorrect_case2(self, sign, c):
+        if sign == '+':
             return c + 2
-        elif symbol == '-':
+        elif sign == '-':
             return c - 2
-        elif symbol == '*':
+        elif sign == '*':
             return c * 2
         else:
             return c / 2
-
-    def calculate(self, symbol1, symbol2, a, b):
-        c = self.operator1(symbol1, a, b)
-        result = self.operator2(symbol2, c)
+        
+    # Calculate results based on the use of incorrect operators
+    def calculate(self, sign1, sign2, a, b):
+        c = self.incorrect_case1(sign1, a, b)
+        result = self.incorrect_case2(sign2, c)
         return result
 
+    # Perform calculations and check the results of operator error cases with the results of the original program
     def checkResultValid(self, value, a, b):
         original_result = self.original_program(a, b)
-        for symbol1 in self.symbols:
-            for symbol2 in self.symbols:
-                if symbol1 == '-' and symbol2 == '*':
+        # use a loop to iterate through all possible erroneous operators in the program 
+        for sign1 in self.signs:
+            for sign2 in self.signs:
+                # Ignore the case if you sign "-" with "*" at the same time because that is the case the program provides 
+                if sign1 == '-' and sign2 == '*':
                     continue
-                result = self.calculate(symbol1, symbol2, a, b)
+                result = self.calculate(sign1, sign2, a, b)
+                # Check whether the results of the test are equal to the results of the original program. If it means this case did not achieve the testing goal 
                 if original_result == result:
-                    value[(a, symbol1, symbol2 , original_result)] = result
+                    # if equal save information of value a , original value, sign1 , sign2 into "value" dictionary 
+                    value[(a, sign1, sign2 , original_result)] = result
         
         return value
 
 
 if __name__ == "__main__":
-    calculation = Calculation()
+    solution = Solution()
     value = {}
+    # iterate through A values ​​from -1000 to 1000 with B = 1 to find A values ​​that do not achieve the testing goal 
     for i in range(-1000, 1001):
-        calculation.checkResultValid(value, i, 1)
+        solution.checkResultValid(value, i, 1)
 
     for val , result in value.items() : 
-        print(f"Original result: {val[3]} ")
-        print(f"Value of A: {val[0]}")
-        print(f"Symbol: {val[1:3]}")
-        print(f"Result: {result} \n")
+        correct_result_str = f"{val[3]:.2f}".rstrip('0').rstrip('.')  
+        incorrect_result_str = f"{result:.2f}".rstrip('0').rstrip('.')  
+        # prints the value of A , the result of true and false and the false operators used 
+        print(f"Result of the correct case: {correct_result_str}, Value of A: {val[0]}, Incorrect operators: {val[1:3]}, Result of the incorrect case: {incorrect_result_str} \n")
+
+
         
